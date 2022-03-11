7670c12182c9095f4432d1e162a84fc4241119c6
unknown <hamzazouaoui10@yahoo.fr>
2022-03-11 16:25:56 +0100

    commit message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Counter from './Counter';
 
-function App() {
-  return (
+class App extends Component {
+  state = { 
+    fullName: 'Hamza Zouaoui',
+    bio: 'I,m am a full stack web developer in Gomycode', 
+    imgSrc:'http://www.parzlogic.com/wp-content/uploads/2017/10/web-dev.jpg', 
+    profession:'Web Developer',
+    show: false  
+  }
+
+  handleShow=()=>{
+    this.setState(
+      {show: !this.state.show}
+    )
+    
+  }
+
+  render() { 
+    return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {this.state.show ? ( 
+      <div className='prof'>
+        <h1>{this.state.fullName}</h1>
+        <h5>{this.state.bio}</h5>
+        <h3>{this.state.profession}</h3>
+        <img src={this.state.imgSrc} alt='webDeveloper' width='30%' />
+        <br />
+      </div> 
+      ):(
+        <h1>Show the profile</h1>
+      )}
+      <button onClick={this.handleShow}>{this.state.show? 'Hide' : 'Show'}</button>
+      <div>
+        <h2>Show the counter</h2>
+        <Counter />
+      </div>
     </div>
-  );
+    )}
 }
-
+ 
 export default App;
